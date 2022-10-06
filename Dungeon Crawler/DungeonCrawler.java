eedde0155451ff8ddad98a3917ebfa2e9d71c552
Danny Goldman <dgoldman@vassar.edu>
2022-10-06 19:03:36 -0400

    Directory Stuff and toString of Game

---
+++ Dungeon Crawler/DungeonCrawler.java
@@ -0,0 +1,6 @@
+public class DungeonCrawler {
+    public static void main(String[] args) {
+        Game test = new Game();
+        System.out.println(test);
+    }
+}
---
+++ Dungeon Crawler/Game.java
@@ -0,0 +1,28 @@
+public class Game {
+    char[][] board;
+    final char avail = 'X';
+    final char occupied = 'E';
+    final char myChar = 'P';
+    public Game() {
+        this.board=  new char[10][10];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                board[i][j] = avail;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        String ret = "";
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                ret += board[i][j] + "  ";
+                if (j == board.length - 1) {
+                    ret += "\n";
+                }
+            }
+        }
+        return ret;
+    }
+}
