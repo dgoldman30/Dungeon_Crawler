8da96bae17c4ae7a3cf4a4038231a1dd8f639ab4
roboj <rolds@vassar.edu>
2022-11-01 11:15:48 -0400

    cont view

--- app/src/main/java/com/example/dungeoncrawler/view/CharCreationView.java
+++ app/src/main/java/com/example/dungeoncrawler/view/CharCreationView.java
@@ -1,5 +1,26 @@
 package com.example.dungeoncrawler.view;
 
-public class CharCreationView implements ICharCreationView{
+import android.content.Context;
+import android.text.Editable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.example.dungeoncrawler.databinding.ActivityMainBinding;
+import com.example.dungeoncrawler.model.Race;
+
+public class CharCreationView implements ICharCreationView {
+    ActivityMainBinding binding;
+    Listener listener;
+
+    public CharCreationView(Context context, Listener listener) {
+        this.binding = ActivityMainBinding.inflate(LayoutInflater.from(context));
+        this.listener = listener;
+
+        
+    }
+
+    @Override
+    public View getRootView() { return this.binding.getRoot();}
 
 }
--- app/src/main/java/com/example/dungeoncrawler/view/ICharCreationView.java
+++ app/src/main/java/com/example/dungeoncrawler/view/ICharCreationView.java
@@ -4,5 +4,11 @@ import android.view.View;
 
 public interface ICharCreationView {
 
+    interface Listener {
+        void onAttIncrease(int index);
+    }
+
+    View getRootView();
+
 
 }
