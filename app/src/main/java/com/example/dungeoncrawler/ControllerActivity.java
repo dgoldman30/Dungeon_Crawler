f05850fcc085211de7ddf501ae9866ab4b886e67
Danny Goldman <Dgoldman@vassar.edu>
2022-11-08 12:03:08 -0500

    just tried to make a clickable 2d array. It could work if the main screen was the array. Commented it for later

--- app/src/main/java/com/example/dungeoncrawler/ControllerActivity.java
+++ app/src/main/java/com/example/dungeoncrawler/ControllerActivity.java
@@ -3,12 +3,13 @@ package com.example.dungeoncrawler;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
 import com.example.dungeoncrawler.databinding.FragmentCharCreationBinding;
-import com.example.dungeoncrawler.model.Caste;
+import com.example.dungeoncrawler.model.*;
 import com.example.dungeoncrawler.model.Game;
 import com.example.dungeoncrawler.model.Player;
 import com.example.dungeoncrawler.model.Race;
@@ -21,6 +22,7 @@ public class ControllerActivity extends AppCompatActivity implements ICharCreati
 
     Game game = new Game(10);
     IMainView mainView;
+    //public Button[][] map = new Button[10][10];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +34,14 @@ public class ControllerActivity extends AppCompatActivity implements ICharCreati
         this.mainView = new MainView(this);
         setContentView(mainView.getRootView());
         mainView.displayFragment(charCreationFragment, false, "char creation");
+        /*for (int i = 0; i < map.length; i++) {
+            for (int j = 0; j < map[i].length; j++) {
+                String buttonID = "button_" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                map[i][j] = findViewById(resID);
+                map[i][j].setOnClickListener((View.OnClickListener) this);
+            }
+        }*/
     }
 
 
